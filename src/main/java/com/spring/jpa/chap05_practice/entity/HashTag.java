bb4359b8ecf18f5e52918c2b4f0344219e67196a
HWoosin <ws1340cc@naver.com>
2023-06-20 15:17:21 +0900

    종합연습예제 페이징 글 수정, 삭제에 대해 공부했습니다. 230619

--- src/main/java/com/spring/jpa/chap05_practice/entity/HashTag.java
+++ src/main/java/com/spring/jpa/chap05_practice/entity/HashTag.java
@@ -23,7 +23,7 @@ public class HashTag {
     private String tagName; //해시태그 이름
 
     //N이 주인! FK를 가진다!
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name="post_no")
     private Post post;
 
